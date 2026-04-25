61dddce5e6be4173d7fbe7db8c8fa319c4f87c18
muthuc9pro2-code <muthuc9pro2@gmail.com>
2026-04-25 12:45:17 +0530

    from first

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ import { useState, useEffect } from 'react';
 import AddItem from './AddItem';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')) || []);
+  const API_URL = ' http://localhost:3500/itemss'
+  const [items, setItems] = useState([])
       const [newItem, setNewItem] = useState('')
       const [search, setSearch] = useState('')
+      const [fetchError, setfetchError] = useState(null)
 
       useEffect(() => {
-        localStorage.setItem('shoppinglist', JSON.stringify(items))
-      },[items])
+       const fetchItems = async() => {
+        try {
+        const response = await fetch(API_URL);
+        if(!response.ok) throw Error ('Did not receive expected data');
+        const listItems = await response.json()
+        console.log(listItems)
+        setItems(listItems)
+        setfetchError(null)
+       } catch(err) {
+        setfetchError(err.message)
+      }
+    }
+    fetchItems();
+  },[])
 
       const addItem = (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -50,11 +64,16 @@ function App() {
         setNewItem={setNewItem}
         handleSubmit={handleSubmit}
       />
+      <main>
+        {fetchError && <p style= {{ color: 'red' }}>
+          {`Error: ${fetchError}`} </p>}
+          {!fetchError && 
       <Content 
       items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
       handleCheck={handleCheck}
       handleDelete={handleDelete}
-      />
+      />}
+      </main>
       <Footer length = {items.length} />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ import ItemList from './ItemList';
 const Content = ({items, handleCheck, handleDelete}) => {
     
   return (
-    <main>
+    <>
         {items.length ? (
           <ItemList 
              items={items}
@@ -17,7 +17,7 @@ const Content = ({items, handleCheck, handleDelete}) => {
                 <p>Your list is empty</p>
             </div>
         )}        
-    </main>
+    </>
   )
 }
 
